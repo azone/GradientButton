d9702cd5d7ed333bdc1ca2a197347e7bfa8274f6
Yozone Wang <wangyaozh@gmail.com>
2019-12-12 23:16:25 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "GradientButton",
     platforms: [
-        .iOS(.v8.0),
-        .tvOS(.v9.0)
+        .iOS(.v8),
+        .tvOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -15,18 +15,12 @@ let package = Package(
             name: "GradientButton",
             targets: ["GradientButton"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "GradientButton",
             dependencies: []),
-        .testTarget(
-            name: "GradientButtonTests",
-            dependencies: ["GradientButton"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5.1]
 )
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# GradientButton
+# 介绍
 
-A description of this package.
+GradientButton 是一个用 Swift 写的 iOS 渐变按钮，之前在上一家公司的时候写的，这两天用 Swift 重写了一下，现在开源出来。
+
+## 安装
+
+
